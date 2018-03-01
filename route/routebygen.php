370e7828f398478db395496837c2775fcf3773ed
limweb <limweb@hotmail.com>
2018-03-01 16:30:09 +0700

    gensystem

--- route/routebygen.php
+++ route/routebygen.php
@@ -1,9 +1,12 @@
 <?php
  $server->addClass('AppController','/api/v3/apps'); 
- $server->addClass('BookController','/api/v3/books'); 
+ $server->addClass('CategoryController','/api/v3/categories'); 
+ $server->addClass('ColorController','/api/v3/colors'); 
  $server->addClass('ColumnController','/api/v3/columns'); 
  $server->addClass('CompanyController','/api/v3/companies'); 
  $server->addClass('DbinfoController','/api/v3/dbinfos'); 
+ $server->addClass('MachineController','/api/v3/machines'); 
+ $server->addClass('MaketoController','/api/v3/maketos'); 
  $server->addClass('MenuController','/api/v3/menus'); 
  $server->addClass('Model_has_permissionController','/api/v3/model_has_permissions'); 
  $server->addClass('Model_has_roleController','/api/v3/model_has_roles'); 
@@ -11,8 +14,14 @@
  $server->addClass('PackageController','/api/v3/packages'); 
  $server->addClass('Password_resetController','/api/v3/password_resets'); 
  $server->addClass('PermissionController','/api/v3/permissions'); 
+ $server->addClass('ProductController','/api/v3/products'); 
  $server->addClass('ProfileController','/api/v3/profiles'); 
  $server->addClass('Role_has_permissionController','/api/v3/role_has_permissions'); 
  $server->addClass('RoleController','/api/v3/roles'); 
+ $server->addClass('SizeController','/api/v3/sizes'); 
+ $server->addClass('StockController','/api/v3/stocks'); 
  $server->addClass('SyspackageController','/api/v3/syspackages'); 
+ $server->addClass('TimeshiftController','/api/v3/timeshifts'); 
+ $server->addClass('UnitController','/api/v3/units'); 
  $server->addClass('UserController','/api/v3/users'); 
+ $server->addClass('WarehouseController','/api/v3/warehouses'); 
