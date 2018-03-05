21ad4967afe2d31ead71b27f3fbe3e2b738309e7
limweb@hotmail.com <limweb@hotmail.com>
2018-03-05 13:55:23 +0700

    merge footer

--- page/themes/lcrm/layout/footer.php
+++ page/themes/lcrm/layout/footer.php
@@ -8,11 +8,7 @@
       Anything you want
     </div>
     <!-- Default to the left -->
-<<<<<<< HEAD
     <strong>Copyright &copy; 2016 <a href="#"><?=$company?></a>.</strong> All rights reserved.
-=======
-    <!-- <strong>Copyright &copy; 2016 <a href="#"><?=$company?></a>.</strong> All rights reserved. -->
->>>>>>> d457d76cc15955b1cf02442ede6ce246fbe5e449
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114317649-1"></script> -->
     <!-- <script>
